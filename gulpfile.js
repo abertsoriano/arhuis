6583a2ed213366ebf376263e00077cb68dd80575
Oscar Larriega <larriega@gmail.com>
2017-07-25 22:39:54 -0500

    Add minify js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
 var cssmin = require('gulp-cssmin');
 var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 
 gulp.task('css', function () {
 	gulp.src(['css/lib/*.css', 'css/dev/*.css'])
 		.pipe(cssmin())
 		.pipe(concat('styles.min.css'))
 		.pipe(gulp.dest('css'));
+});
+
+gulp.task('minify', function() {
+	gulp.src('js/*.js')
+		.pipe(minify({
+			ext:{
+				min:'.js'
+			}
+		}))
+		.pipe(gulp.dest('js/app/'))
 });
